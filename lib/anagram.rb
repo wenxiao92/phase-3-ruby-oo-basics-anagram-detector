a3e093fe51155f9470ed096958dcff515668e1e6
Wen Xiao <wennstyle@gmail.com>
2022-04-18 20:35:42 -0400

    commit tree c00c512f1965606a4eb5498937c4d5356789fce6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+    @word = word
+    end
+
+    def match(array)
+        array.select {|x| x.chars.sort == @word.chars.sort}
+    end
+end
